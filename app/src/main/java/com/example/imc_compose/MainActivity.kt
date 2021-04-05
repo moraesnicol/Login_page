6f117f01c20a996d74891a8f9f274c140a0868f0
Gabriel Nicol <moraesnicol@gmail.com>
2021-04-04 21:07:38 -0300

    LoginPage init

--- app/src/main/java/com/example/imc_compose/MainActivity.kt
+++ app/src/main/java/com/example/imc_compose/MainActivity.kt
@@ -1,48 +1,33 @@
 package com.example.imc_compose
 
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.example.imc_compose.composables.LoginPage1
 import com.example.imc_compose.ui.theme.IMC_COMPOSETheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val windows = this.window
+        windows.statusBarColor = Color.WHITE
+
         setContent {
             IMC_COMPOSETheme {
 
-                   NovaHistoria()
+                   Surface(color = MaterialTheme.colors.background) {
+                       LoginPage1()
+
+                   }
                 }
             }
         }
     }
 
 
-@Composable
-fun NovaHistoria() {
-    Column (
 
-
-            ){
-
-        Text("Hello ", Modifier.padding(top = 32.dp, bottom = 20.dp, start = 90.dp), )
-        Text("VÁVÁ")
-        Text("Saudades")
-    }
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFF009688, widthDp = 140, heightDp = 60)
-@Composable
-fun DefaultPreview() {
-    IMC_COMPOSETheme {
-        NovaHistoria()
-    }
-}
--- app/src/main/java/com/example/imc_compose/composables/LoginPage1.kt
+++ app/src/main/java/com/example/imc_compose/composables/LoginPage1.kt
@@ -1,4 +1,12 @@
 package com.example.imc_compose.composables
 
-class LoginPage1 {
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPage1() {
+    Text(text = "Bem vindo ao universo Compose ;D")
 }
